5a7077e855995f129ea691ae41c7ea5b134bff28
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-13 20:34:27 -0600

    docs(if.py): explains the functionality of the code

--- if.py
+++ if.py
@@ -1,5 +1,7 @@
+#We receive a value in the console and store it in the variable "x"
 x = int(input("Please enter an integer: "))
 
+#We check the value of "x" and print a message accordingly
 if x < 0:
     x = 0
     print('Negative changed to zero')
