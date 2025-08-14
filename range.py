46fb6d20a48775d5653818964edb9271cef8680e
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-14 07:03:49 -0600

    docs: more codes example of the range function

--- range.py
+++ range.py
@@ -7,3 +7,18 @@ for i in range(5):
 # 2
 # 3
 # 4
+
+# Generate a list of numbers from 5 to 9:
+list(range(5, 10))
+#response
+# [5, 6, 7, 8, 9]
+
+# Generate a list of numbers from 0 to 9 with a step of 3:
+list(range(0, 10, 3))
+#response
+#[0, 3, 6, 9]
+
+# Generate a list of numbers from -10 to -100 with a step of -30:
+list(range(-10, -100, -30))
+#response
+#[-10, -40, -70]
