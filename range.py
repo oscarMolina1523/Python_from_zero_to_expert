4f1f1b0c83bcd013254e9d82ada9974f9b6f4b98
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-14 18:30:19 -0600

    feat: special uses cases

--- range.py
+++ range.py
@@ -22,3 +22,16 @@ list(range(0, 10, 3))
 list(range(-10, -100, -30))
 #response
 #[-10, -40, -70]
+
+#This is a strange case because if you try to 
+#print only one range it will give an unexpected result.
+range(10)
+
+#response
+#range(0, 10)
+
+#but if you use it inside a function it is different
+sum(range(4))  # 0 + 1 + 2 + 3
+
+#response
+# 6
