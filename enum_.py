06563d22ab3234e5d47a96e2a7b44352d0882917
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-15 20:05:58 -0600

    docs: explain how enum statement works

--- enum_.py
+++ enum_.py
@@ -1,16 +1,21 @@
+#import Enum from enum module
 from enum import Enum
 
+# Define an enumeration for colors
+#recives a string and returns the corresponding Enum member
+#always use Enum parameter
 class Color(Enum):
     RED = 'red'
     GREEN = 'green'
     BLUE = 'blue'
 
+# Get user input
 user_input = input("Enter your choice of 'red', 'blue' or 'green': ")
 
 try:
-    color = Color(user_input)   # Intentamos convertir el string a Enum
+    color = Color(user_input)   # try to convert input to Color Enum
 except ValueError:
-    print("❌ Unknown color")
+    print("❌ Unknown color") # handle invalid input
 else:
     match color:
         case Color.RED:
