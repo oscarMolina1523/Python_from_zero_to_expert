f202d4e5221cb0519a837924170a04dde26e5847
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-16 18:10:02 -0600

    feat: the code use an array to save the result

--- def_.py
+++ def_.py
@@ -14,3 +14,20 @@ fib(2000)
 #response
 # Ejecutando def_.py
 # 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 15
+
+#second example using array to save the results
+# The function fib2 is similar to fib, but it returns the results as a list instead
+def fib2(n):  # return Fibonacci series up to n
+    """Return a list containing the Fibonacci series up to n."""
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)    # see below
+        a, b = b, a+b
+    return result
+
+print('Ejecutando def_.py fib2')
+
+print(fib2(100))   # call it
+ # write the result
+# [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
