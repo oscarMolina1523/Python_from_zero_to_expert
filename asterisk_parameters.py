fe9dd606af32ce9b589a3513129d97a24a99e908
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-18 15:31:06 -0600

    docs: explain how does asterisk parameters works

--- asterisk_parameters.py
+++ asterisk_parameters.py
@@ -1,12 +1,24 @@
+# create a function that takes different types of parameters
+# the var kind is a required parameter (normal parameter)
+# the *arguments is a variable-length argument list (tuple)
+#Captures any number of unnamed (positional) extra parameters passed. Saved as a tuple.
+# the **keywords is a variable-length keyword argument list (dictionary)
+#Captures any number of named parameters (key=value).Saved as a dictionary.
+
 def cheeseshop(kind, *arguments, **keywords):
     print("-- Do you have any", kind, "?")
     print("-- I'm sorry, we're all out of", kind)
+    # loop through the array using a for loop
     for arg in arguments:
         print(arg)
     print("-" * 40)
+    # loop through the dictionary using a for loop
     for kw in keywords:
+        # print the key and value
+        # keywords[kw] is the value of the key
         print(kw, ":", keywords[kw])
 
+# Example usage of the cheeseshop function
 cheeseshop("Limburger", "It's very runny, sir.",
            "It's really very, VERY runny, sir.",
            shopkeeper="Michael Palin",
