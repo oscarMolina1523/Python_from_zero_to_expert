b2f049fbbc89505777901c6917c7ace9a669dc41
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-18 18:27:52 -0600

    docs: explaining how does lambda function works

--- lambda.py
+++ lambda.py
@@ -1,10 +1,15 @@
+#create a function that returns a function
+#this receives a number and returns a function that adds that number to its argument
 def make_incrementor(n):
-    return lambda x: x + n
+    #this is a lambda function that takes an argument x and adds n to it
+    return lambda x: x + n #here n is captured from the make_incrementor function's scope
+                           #return a function like this: f(x) = x + n |<--n is the captured variable
+                           #in the next line n=42, so the returned function will be f(x) = x + 42
 
-f = make_incrementor(42)
-print(f(0))
+f = make_incrementor(42) #here return the function: f(x) = x + 42
+print(f(0)) #here is passing 0 to the function f(x) = x + 42, so it will return 42
 #response
 #42
-print(f(1))
+print(f(1)) #here is passing 1 to the function f(x) = x + 42, so it will return 43
 #response
 #43
