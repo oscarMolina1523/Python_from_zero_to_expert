db3361d2c1750039755097c69559560eaaed31a3
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-19 14:03:31 -0600

    docs: explain how does annotation function works

--- annotation_functions.py
+++ annotation_functions.py
@@ -1,8 +1,13 @@
+#create a functiion, it receives two arguments, one is mandatory and one with a default value
+#and returns a string 
 def f(ham: str, eggs: str = 'eggs') -> str:
+    #you can access the annotations of the function using the __annotations__ attribute
     print("Annotations:", f.__annotations__)
     print("Arguments:", ham, eggs)
+    #that concatenates the two arguments
     return ham + ' and ' + eggs
 
+#example of how to call the function
 f('spam')
 
 #response
