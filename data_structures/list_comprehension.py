08d4766bed80e156f746730a1bf35c1a1eece7c5
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-19 18:21:01 -0600

    feat: execute a list using list comprehesion and writting less code

--- data_structures/list_comprehension.py
+++ data_structures/list_comprehension.py
@@ -1,3 +1,4 @@
+#FIRST CASE OF STUDY
 #create an empty list
 squares = []
 #execute a for loop to append the square of each number from 0 to 9
@@ -10,3 +11,17 @@ for x in range(10):
 print(squares)
 #output
 #[0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+
+
+#SECOND CASE OF STUDY
+#using list comprehension we can create the same list in a single line
+#first we create the list using the [list] keyword
+squares2 = list(
+    map( #using the map function to applies a function to each element of an iterable
+        lambda x: x**2, #Lambda is an anonymous function: it receives a value (x) and returns x squared
+        range(10) #range(10) generates numbers from 0 to 9
+    )
+)
+
+
+print(squares2)  # Output: [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
