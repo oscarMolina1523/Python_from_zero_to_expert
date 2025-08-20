8691ec251c99366a1128dc472b8bf16a3242581e
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-20 06:56:07 -0600

    docs: explain how does second use case works

--- data_structures/double_for_loop.py
+++ data_structures/double_for_loop.py
@@ -7,11 +7,11 @@
 #[(1, 3), (1, 4), (2, 3), (2, 1), (2, 4), (3, 1), (3, 4)]
 
 #SECOND USE CASE
-result = []
-for x in [1,2,3]:
-    for y in [3,1,4]:
-        if x != y:
-            result.append((x, y))
+result = [] # create an empty list to store results
+for x in [1,2,3]: #iterate the first list in x variable
+    for y in [3,1,4]: #iterate the second list in y variable
+        if x != y: #if the var x is different than y
+            result.append((x, y)) #append the tuple (x,y) to the result list
 
 #output
 #[(1, 3), (1, 4), (2, 3), (2, 1), (2, 4), (3, 1), (3, 4)]
