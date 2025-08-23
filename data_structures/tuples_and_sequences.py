37a62468286fc910fdd541078c93808724d06d14
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-23 11:03:47 -0600

    feat: create a tuple without parentheses

---
+++ data_structures/tuples_and_sequences.py
@@ -0,0 +1,4 @@
+#tuples are inmutable sequences, but can contain mutable objects
+#tuples use parentheses, but they can be created without them
+t = 12345, 54321, 'hello!' #tuple without parentheses
+print(t[0]) #output: 12345
