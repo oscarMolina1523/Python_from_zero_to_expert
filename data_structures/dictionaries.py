2272c95a9f481328afdb2a9194718c9875a35b59
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-23 14:21:58 -0600

    feat: create a lust with the values of a dictionary

--- data_structures/dictionaries.py
+++ data_structures/dictionaries.py
@@ -12,3 +12,10 @@ print(tel['jack']) #output: 4098
 #you can delete an item by its key
 del tel['sape'] #sape is the key
 print(tel) #output: {'jack': 4098, 'guido': 4127}
+
+#you can create a list with the keys of a dictionary
+#by default, iterating a dictionary iterates over its keys
+print(list(tel)) #output: ['jack', 'guido']
+print(list(tel.keys()))   # ['jack', 'guido']
+print(list(tel.values())) # [4098, 4139]
+print(list(tel.items()))  # [('jack', 4098), ('guido', 4139)]
