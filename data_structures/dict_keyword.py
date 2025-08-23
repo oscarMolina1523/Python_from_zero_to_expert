fd088a0d0fae78f0509abb53f932567c12b97e61
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-23 14:32:06 -0600

    feat: declare a dictionary using second case

--- data_structures/dict_keyword.py
+++ data_structures/dict_keyword.py
@@ -2,3 +2,7 @@
 #dictionaries are mutable, unordered collections of key-value pairs
 first_dict = dict([('sape', 4139), ('guido', 4127), ('jack', 4098)])
 print(first_dict) #output: {'sape': 4139, 'guido': 4127, 'jack': 4098}
+
+#SECOND CASE
+second_dict = dict(sape=4139, guido=4127, jack=4098)
+print(second_dict) #output: {'sape': 4139, 'guido': 4127, 'jack': 4098}
