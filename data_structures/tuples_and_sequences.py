01b1b10618bf0f84fce793403309e04c79b73d85
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-23 11:08:13 -0600

    docs: tuples are inmutables

--- data_structures/tuples_and_sequences.py
+++ data_structures/tuples_and_sequences.py
@@ -7,3 +7,10 @@ print(t) #output: (12345, 54321, 'hello!')
 #tuples may be nested
 u = t, (1, 2, 3, 4, 5) #nested tuple
 print(u) #output: ((12345, 54321, 'hello!'), (1, 2, 3, 4, 5))
+
+#tuples are inmutables, you cannot modify them
+# t[0] = 88888 
+# #if you want do this, you will get an error
+#Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# TypeError: 'tuple' object does not support item assignment
