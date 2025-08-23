c77c29280440ba87b246df2032c81f341e9aa655
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-23 09:08:28 -0600

    docs: explain how does del instruction works

--- data_structures/del_instruction.py
+++ data_structures/del_instruction.py
@@ -1,3 +1,5 @@
+#create a list of numbers
 a = [-1, 1, 66.25, 333, 333, 1234.5]
-del a[0]
+#using [del] instruction to remove the first element of the list
+del a[0] #the number in position 0 is -1
 print(a) #output: [1, 66.25, 333, 333, 1234.5]
