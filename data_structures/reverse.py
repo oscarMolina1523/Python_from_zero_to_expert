b8a1feb7763fd31658ed0a5c822c7aed75e6ea90
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-23 15:45:20 -0600

    feat: code example to explain how does reversed function works

---
+++ data_structures/reverse.py
@@ -0,0 +1,11 @@
+#using a for loop and 
+#the reversed() function to iterate over a range of numbers in reverse order
+#in this case from 9 to 1 with a step of 2
+for i in reversed(range(1, 10, 2)):
+    print(i)
+#output:
+#9
+#7
+#5
+#3
+#1
