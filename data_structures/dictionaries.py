ebed766735699a429e7bb6aeeb5af32ba3e4f2a0
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-23 14:18:34 -0600

    feat: delete a item by its key

--- data_structures/dictionaries.py
+++ data_structures/dictionaries.py
@@ -8,3 +8,7 @@ print(tel) #output: {'jack': 4098, 'sape': 4139, 'guido': 4127}
 
 #tou can receive the value of a key
 print(tel['jack']) #output: 4098
+
+#you can delete an item by its key
+del tel['sape'] #sape is the key
+print(tel) #output: {'jack': 4098, 'guido': 4127}
