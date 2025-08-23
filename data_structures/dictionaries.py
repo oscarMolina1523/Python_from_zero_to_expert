1d73fdf6f7b5c62eaae610a3062c83bacdd56a89
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-23 14:16:56 -0600

    feat: obtain value of a key in dictionary

--- data_structures/dictionaries.py
+++ data_structures/dictionaries.py
@@ -5,3 +5,6 @@ tel = {'jack': 4098, 'sape': 4139}
 #you can add a new item
 tel['guido'] = 4127
 print(tel) #output: {'jack': 4098, 'sape': 4139, 'guido': 4127}
+
+#tou can receive the value of a key
+print(tel['jack']) #output: 4098
