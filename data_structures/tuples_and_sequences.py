2b1d8b8394a840e4c7656bedceb620ae3c815153
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-23 11:06:29 -0600

    feat: code example , tuples nested

--- data_structures/tuples_and_sequences.py
+++ data_structures/tuples_and_sequences.py
@@ -2,3 +2,8 @@
 #tuples use parentheses, but they can be created without them
 t = 12345, 54321, 'hello!' #tuple without parentheses
 print(t[0]) #output: 12345
+print(t) #output: (12345, 54321, 'hello!')
+
+#tuples may be nested
+u = t, (1, 2, 3, 4, 5) #nested tuple
+print(u) #output: ((12345, 54321, 'hello!'), (1, 2, 3, 4, 5))
