984d33b98ec7f0527f60401e4629042bc0cb9623
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-23 14:30:29 -0600

    feat: declare a dictionary using [dict] keyword

---
+++ data_structures/dict_keyword.py
@@ -0,0 +1,4 @@
+#you can create a dictionary using the dict keyword
+#dictionaries are mutable, unordered collections of key-value pairs
+first_dict = dict([('sape', 4139), ('guido', 4127), ('jack', 4098)])
+print(first_dict) #output: {'sape': 4139, 'guido': 4127, 'jack': 4098}
