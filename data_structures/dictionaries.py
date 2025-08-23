afe65a47543cada6d52676a8626590b568f99e98
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-23 13:13:32 -0600

    feat: introduction to distionaries

---
+++ data_structures/dictionaries.py
@@ -0,0 +1,7 @@
+#create a dictionary
+#dictionaries are mutable mappings, they map keys to values
+tel = {'jack': 4098, 'sape': 4139}
+
+#you can add a new item
+tel['guido'] = 4127
+print(tel) #output: {'jack': 4098, 'sape': 4139, 'guido': 4127}
