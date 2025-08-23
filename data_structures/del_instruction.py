18c30644c96445ac56a153ae08e3c8ded05feda5
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-23 10:51:47 -0600

    feat: delete a range of numbers

--- data_structures/del_instruction.py
+++ data_structures/del_instruction.py
@@ -3,3 +3,6 @@ a = [-1, 1, 66.25, 333, 333, 1234.5]
 #using [del] instruction to remove the first element of the list
 del a[0] #the number in position 0 is -1
 print(a) #output: [1, 66.25, 333, 333, 1234.5]
+
+del a[2:4] #removes the elements in positions 2 and 3
+print(a) #output: [1, 66.25, 1234.5]
