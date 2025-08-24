5b45e5c0ac1eb35b8ae63be404252f39d3c8f1c4
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-23 18:27:37 -0600

    feat: assign value of function to local var

--- modules/main.py
+++ modules/main.py
@@ -5,3 +5,7 @@ print(fibo.fib2(1000))
 
 #__name__ variable is for know the name of the module
 print(fibo.__name__)  # prints 'fibo'
+
+#if you want to use a function with frequency, you can assign it to a local name
+fib = fibo.fib
+fib(500)
