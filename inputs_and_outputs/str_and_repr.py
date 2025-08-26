5a488068d0960110bb180a2ad5059529b9f496ce
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-26 14:19:32 -0600

    feat: code example difference between str and repr

--- inputs_and_outputs/str_and_repr.py
+++ inputs_and_outputs/str_and_repr.py
@@ -2,3 +2,9 @@
 #repr() function need context to show the value
 #if you dont pass context to repr() it will show the literal value
 
+s='Hello World'
+
+#we can see the difference between str() and repr()
+#str show the value normal, but the repr show the value with quotes and double quotes
+print(str(s)) #output: 'Hello World'
+print(repr(s)) #output: "'Hello World'"
