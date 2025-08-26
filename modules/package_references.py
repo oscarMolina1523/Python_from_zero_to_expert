86c1012f0921cf0c98e0271f605f5b03db000aac
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-25 20:26:18 -0600

    feat: basic import level

---
+++ modules/package_references.py
@@ -0,0 +1,13 @@
+# mipaquete/
+#  ├── __init__.py
+#  ├── sonido/
+#  │    ├── __init__.py
+#  │    ├── echo.py
+#  │    └── efectos.py
+
+#think you have a package structure like this
+
+#this import is similar like this in javascript 
+#import echo from "./echo.js";
+from . import echo
+
